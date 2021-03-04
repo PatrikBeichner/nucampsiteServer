c8e623507e07774499bb5ef5f3f290dfa524e3a8
PatrikBeichner <patrik.beichner@gmail.com>
2021-03-03 22:28:54 -0500

    Formatting/comments

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 const passport = require('passport');
 const config = require('./config');
 
+//routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const campsiteRouter = require('./routes/campsiteRouter');
